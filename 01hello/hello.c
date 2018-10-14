918bf0fc3881ae1d777b721559c90290c6ae3e4c
Kenjiro Taura <taura.kenjiro@gmail.com>
2018-10-15 01:20:04 +0900

    hello

--- 01hello/hello.c
+++ 01hello/hello.c
@@ -1,13 +1,16 @@
 /**
    @file hello.c
   */
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <sched.h>
 #include <omp.h>
 
 void worker() {
   int rank = omp_get_thread_num();
   int nthreads = omp_get_num_threads();
-  printf("hello from thread %d of %d\n", rank, nthreads);
+  int cpu = sched_getcpu();
+  printf("hello from thread %d of %d on %d\n", rank, nthreads, cpu);
 }
 
 int main() {
