8839f889370397e967627cb5985329225544269d
tau@eidos.ic.i.u-tokyo.ac.jp <tau@logos.t.u-tokyo.ac.jp>
2018-11-04 21:17:47 +0900

    rename

--- 05simd/09random_store.c
+++ 05simd/09random_store.c
@@ -1,7 +1,7 @@
 #include <assert.h>
 
-void loop_indirect(float a, float * restrict x, long * idx, float b,
-                   float * restrict y, long n) {
+void loop_random_store(float a, float * restrict x, long * idx, float b,
+                       float * restrict y, long n) {
   /* tell the compiler x and y are 64 bytes-aligned (a multiple of 64) */
   x = __builtin_assume_aligned(x, 64);
   y = __builtin_assume_aligned(y, 64);
